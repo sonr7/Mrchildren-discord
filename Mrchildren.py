da53a4478e72032df3ab93c159405f0aea8df9a6
shinonomeshion <78985521+shinonomeshion@users.noreply.github.com>
2021-03-14 17:48:00 +0900

    Update Mrchildren.py

--- Mrchildren.py
+++ Mrchildren.py
@@ -7,13 +7,13 @@ token = os.environ.get('DISCORD_BOT_TOKEN')
 client = discord.Client()
 
 with open('kasi.txt') as f:
-    kasi_dict = {line.split(':')[0]: line[1] for line in f.readlines()}
+    kasi_dict = {line.split(':')[0]: line[-1] for line in f.readlines()}
 
 @client.event
 async def on_message(message):
     if '#Mr.Children' in message.content and message.content.startswith('#'):
         kasi = message.content.replace('#Mr.Children', '')
-        await message.channel.send(kasi_dict[kasi])
+        await message.channel.send(kasi_dict)
                  
                  
       
