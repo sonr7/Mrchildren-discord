f81dc819b0232230359a67422b6e08de9d470e9a
shinonomeshion <78985521+shinonomeshion@users.noreply.github.com>
2021-03-14 14:02:20 +0900

    Update Mrchildren.py

--- Mrchildren.py
+++ Mrchildren.py
@@ -11,8 +11,8 @@ with open('kasi.txt') as f:
 
 @client.event
 async def on_message(message):
-    if '#ミスチル' in message.content and message.content.startswith('#'):
-        kasi = message.content.replace('#ミスチル')
+    if '#Mr.Children' in message.content and message.content.startswith('#'):
+        kasi = message.content.replace('#Mr.Children')
         await message.channel.send(kasi_dict[kasi])
                  
                  
